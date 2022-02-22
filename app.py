d54597671fea157a1a337d3decc44ba59b1e0f31
unknown <angell_2104@hotmail.com>
2022-02-22 11:28:05 -0600

    route/users was added

--- app.py
+++ app.py
@@ -1,7 +1,37 @@
-from flask import Flask
-from Flask_PyMongo import Pymongo
+from flask import Flask, request
+from flask_pymongo import PyMongo
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+app.config['MONGO_URI']= 'mongodb://localhost/pymongodb'
+
+mongo= PyMongo(app)
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    username= request.json['username']
+    email= request.json['email']
+    password= request.json['password']
+    
+    
+    if username and email and password:
+        hashed_password=generate_password_hash(password)
+        id=mongo.db.users.insert(
+            {'username': username, 'email': email, 'password': hashed_password }
+        )
+        response={
+            'id': str(id),
+            'username': username,
+            'email': email,
+            'password': hashed_password
+        }
+        return response
+    else:
+        {'message': 'received'}
+        
+    
+    return {'message': 'received'}
+
 
 if __name__ == '__main__':
     app.run(debug=True)
